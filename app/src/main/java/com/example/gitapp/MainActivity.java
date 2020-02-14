410936833c84a419e4e46554a3f1ccbeeee99661
Javier Miranda <jmiranda@iuma.ulpgc.es>
2020-02-14 10:59:01 +0000

    T01: Initial Screen
    Adding documentation to my initial screen.

--- app/src/main/java/com/example/gitapp/MainActivity.java
+++ app/src/main/java/com/example/gitapp/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Documentacion inicial de onCreate
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
